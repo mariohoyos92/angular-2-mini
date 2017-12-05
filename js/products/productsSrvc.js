44ea0cfb204024d9501fefacfc4f3e0ca65393c8
Mario Hoyos <mariohoyos92@gmail.com>
2017-12-05 15:09:12 -0600

    chore: completed the challenge/tutorial

--- js/products/productsSrvc.js
+++ js/products/productsSrvc.js
@@ -1,3 +1,17 @@
-angular.module('myApp').service('productsSrvc', function() {
+angular.module("myApp").service("productsSrvc", function($http) {
+  var baseUrl = "https://practiceapi.devmountain.com/products";
 
+  this.getShoeData = function() {
+    return $http({
+      method: "GET",
+      url: baseUrl + "?category=shoes"
+    });
+  };
+
+  this.getSockData = function() {
+    return $http({
+      method: "GET",
+      url: baseUrl + "?category=socks"
+    });
+  };
 });
